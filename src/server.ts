2f4166282f7f9e5d1a6039bdf850bcb671600757
biruta-san <ryanhenriq2019@gmail.com>
2024-09-29 21:21:35 -0300

    Configurações vercel

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,11 @@ import app from "./app";
 import { swaggerDocs } from "./swagger/swaggerConfig";
 
 // Porta da aplicação
+const URL: string = process.env.APP_URL || "http://localhost";
 const PORT: number = parseInt(process.env.PORT || "3000", 10) || 3000;
 
 // Inicializando o servidor
 app.listen(PORT, () => {
   // Documentação do swagger
-  swaggerDocs(app, PORT);
+  swaggerDocs(app, URL, PORT);
 });
--- src/swagger/swaggerConfig.ts
+++ src/swagger/swaggerConfig.ts
@@ -41,7 +41,6 @@ const options = {
 
 const swaggerSpec = swaggerJsdoc(options);
 
-export function swaggerDocs(app: Express, port: number) {
+export function swaggerDocs(app: Express, url: string, port: number) {
   app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-  console.log(`Swagger docs available at http://localhost:${port}/api-docs`);
 }
